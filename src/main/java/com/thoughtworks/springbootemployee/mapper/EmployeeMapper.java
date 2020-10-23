5f906ef76bd7ba7e2fbfbe25ed64dcf5eb5be951
Hannah Mae Reyes <hannah.mae.reyes@oocl.com>
2020-10-23 16:11:50 +0800

    [SpringBootEmployee] [Jeyem & Hannah] add: added Component annotation

--- src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
@@ -4,7 +4,9 @@ import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.model.EmployeeRequest;
 import com.thoughtworks.springbootemployee.model.EmployeeResponse;
 import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmployeeMapper {
     public EmployeeResponse toResponse (Employee employee) {
         EmployeeResponse employeeResponse = new EmployeeResponse();
